c2e47ca9d19d88daa30e040b6f35bb969acbbd7e
violeta brizuela <violeta brizuela>
2016-05-03 11:52:46 -0700

    added a rule

--- Project4/project4.pl
+++ Project4/project4.pl
@@ -1,8 +1,10 @@
 %  Problem2
 % hello
+
 lower_letter(X):- member(X,[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
 
 f([X]):- number(X).
+f([X]):- lower_letter(X).
 f(X):- append(['('|List],[')'],X), e(List).
 
 
@@ -13,8 +15,6 @@ e(X):- append([],List,X), t(List).
 t(X):- append(List1,[*|List2],X), t(List1), f(List2).
 t(X):- append(List1,[/|List2],X), t(List1), f(List2).
 t(X):- append([],List,X), f(List).
-
-
 
 
 
